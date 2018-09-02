ea07bd0c1808a6f5d509e1241e1760d4466a6054
y-asai <ryuta234.you@gmail.com>
2018-09-02 15:57:10 +0900

    翻訳 処理入れて見た

--- bot_sample/call_back/call_back.php
+++ bot_sample/call_back/call_back.php
@@ -13,6 +13,19 @@ use \LINE\LINEBot\Constant\HTTPHeader;
 //LINESDKの読み込み
 require_once(__DIR__."/../../vendor/autoload.php");
 
+// まずあらかじめ翻訳するための認証キーをAzureポータルサイトから
+// 取得しておき、以下のKEY_1と置き換えます
+$azure_key = "351118560b1a45929f0d91492722b4af";
+// 翻訳元言語です
+// 翻訳可能な言語の一覧は以下のブログ記事に載っています
+// http://ken-tokyo.hatenablog.com/entry/2017/12/08/150212
+$fromLanguage = "en";  
+// 翻訳先言語です
+$toLanguage = "ja";
+// 翻訳する英文です
+$inputStr = "This";
+
+
 $list = array(
     "大吉",
     "中吉",
@@ -28,7 +41,7 @@ if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
     //LINEBOTにPOSTで送られてきた生データの取得
     $inputData = file_get_contents("php://input");
     
-    error_log("y--------" . print_r($inputData,true));
+    error_log("inputData-------- : " . print_r($inputData,true));
     
     //LINEBOTSDKの設定
     $httpClient = new CurlHTTPClient(ACCESS_TOKEN);
@@ -42,7 +55,7 @@ if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
      
     $Events = $Bot->parseEventRequest($inputData, $signature);
     
-    error_log("y--------" . print_r($Events,true));
+    error_log("Events-------- : " . print_r($Events,true));
     
     //大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
     foreach ($Events as $event) {
@@ -50,13 +63,67 @@ if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
 //            $logger->info('Non text message has come');
             continue;
         }
-        error_log("y--------" . print_r($event->getText(), true));
+        error_log("InputText-------- : " . print_r($event->getText(), true));
+        // 入力文字
+        $input_text = $event->getText();
+        
+        // 翻訳処理
+        // 翻訳するためのトークンを取得します。有効期限は取得後10分間です
+        $accessToken = getToken($azure_key);
+
+           // 翻訳するための文字列を生成します
+        $params = "text=" . urlencode($input_text) . "&to=" . $toLanguage . "&from=" . $fromLanguage . "&appId=Bearer+" . $accessToken;
+
+        // 翻訳するためのURLを生成します
+        $translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?$params";
+
+        // 翻訳を実行します
+        $curlResponse = curlRequest($translateUrl);
+
+        // 翻訳結果はxmlで帰ってくるのでそれを読み込みます
+        $translatedStr = simplexml_load_string($curlResponse);
+
         $SendMessage = new MultiMessageBuilder();
-        $TextMessageBuilder = new TextMessageBuilder($event->getText());
+        $TextMessageBuilder = new TextMessageBuilder($translatedStr);
         $SendMessage->add($TextMessageBuilder);
         $Bot->replyMessage($event->getReplyToken(), $SendMessage);
     }
 }
+// 翻訳するためのトークンを取得する関数です
+// このトークンの有効期限は、取得してから10分間とかなり短めです
+function getToken($azure_key)
+{
+    $url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
+    $ch = curl_init();
+    $data_string = json_encode('{body}');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string),
+            'Ocp-Apim-Subscription-Key: ' . $azure_key
+        )
+    );
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $strResponse = curl_exec($ch);
+    curl_close($ch);
+    return $strResponse;
+}
+
+// 翻訳を実行する関数です
+function curlRequest($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $curlResponse = curl_exec($ch);
+    curl_close($ch);
+    return $curlResponse;
+}
+
 //$accessToken = 'QMWZXjlgVbI1eyWO+nlH7Q0VBWc/fnnlDVppIkcAwKeoPgQps8HuibW4W16nDgpi/HS4+1bcrRIxqDyDclsfUrQC8Gkyxiv/+Qi33dDqos1nGRsN0F18y5e5xGVEaNrul0BwJE2lKX6AFn0YArEfSQdB04t89/1O/w1cDnyilFU=';
 // 
 ////ユーザーからのメッセージ取得
